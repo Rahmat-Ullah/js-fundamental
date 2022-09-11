a99f85c914600de71c728b60ad031453a26f87ea
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-11 20:52:04 +0600

    second problem solved

--- problem-solving-array2.js
+++ problem-solving-array2.js
@@ -7,3 +7,18 @@ var johnScore = 40;
 if(myScore >= 80){
     console.log('Your grade is A');
 }
+else if(myScore >= 60){
+    console.log('Your grade is B');
+}
+else if(myScore >= 50){
+    console.log('Your grade is C');
+}
+else if(myScore >= 40){
+    console.log('Your grade is D');
+}
+else if(myScore <= 39){
+    console.log('Your grade is F');
+}
+else{
+    console.log('You were absent in the examination');
+}
