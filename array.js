9095269ca2bb836eaaf3bf94f49ea9ab297bc9e9
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-11 09:58:28 +0600

    array in js practiced

---
+++ array.js
@@ -0,0 +1,7 @@
+var friendsAge = [11, 12, 13, 34, 45, 56, 78, 79];
+var picnicFee = [300,400,20,19,540];
+var players = ['liton', 'miraj', 'mushi', 'tamim'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(vowels);
+console.log(players.length);
---
+++ variable.js
@@ -0,0 +1,9 @@
+// numeric type variable
+var sunglassPrice = 120;
+
+//string type variable
+var name = "Rahmat Ullah";
+
+//boolean type
+var isSingle = true;
+
