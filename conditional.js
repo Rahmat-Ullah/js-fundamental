10e939235a299f3165aa3fc88b135595f2fc888b
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-11 12:51:18 +0600

    condition in js practiced

---
+++ conditional.js
@@ -0,0 +1,15 @@
+var iphonePrice = 89000;
+var myBudget = 9800;
+
+if(iphonePrice<myBudget){
+    console.log("I am buying Iphone");
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+if(chickenPrice<=myMoney){
+    console.log('murgir ran khamu and haddi chabamu');
+}
+else{
+    console.log('smash potato and letus pata');
+}
