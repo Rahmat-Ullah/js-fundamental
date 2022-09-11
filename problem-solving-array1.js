1f36bb0bf8cb1ec8ceb08465a94f3f3a6951c400
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-11 20:04:05 +0600

    first problem solved

---
+++ problem-solving-array1.js
@@ -0,0 +1,11 @@
+//Problem 1
+var fruits = ['Apple', 'Banana', 'Orange'];
+// var index = fruits.indexOf('Banana');
+console.log(fruits.indexOf('Banana'));
+// fruits[1] = 'Mango';
+fruits[1] = 'Mango';
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.push('Watermelon');
+console.log(fruits);
