8d313963546979084685f828871312c0a7d151b3
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-11 19:44:42 +0600

    nested if else practiced

---
+++ nested-condition.js
@@ -0,0 +1,19 @@
+var math = true;
+var geometry = true;
+
+if(math== true){
+    if(geometry == true){
+        if(straightLine == true){
+
+        }
+        else{
+            
+        }
+    }
+    else{
+        console.log('tumi pithagoras hote parbana.');
+    }
+}
+else{
+
+}
