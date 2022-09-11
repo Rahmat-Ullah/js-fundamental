390690a24e994fb692b9872356c94b9fc29151d3
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-11 18:09:24 +0600

    more condition practiced

---
+++ multi-stage-condition.js
@@ -0,0 +1,14 @@
+var money = 20;
+var danishPrice = 45;
+var buuterBread = 23;
+var toastBiscuit = 45;
+
+if(danishPrice < money){
+    console.log('dan dan danish khamu');
+}
+else if(buuterBread < money){
+    console.log('butterbon khamu');
+}
+else{
+    console.log('chubai chubai toast biscuit khamu');
+}
