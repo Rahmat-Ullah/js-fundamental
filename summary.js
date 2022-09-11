ff99c27beca1a45b40c5f8f03b38e6efbf51c1f9
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-11 19:50:24 +0600

    summary of array added

---
+++ summary.js
@@ -0,0 +1,18 @@
+var destination = ['sajek', 'coxbazar', 'saint matrin', 'bandorban'];
+
+console.log(destination);
+destination[2] = 'thailand';
+var index = destination.indexOf('bandorban');
+
+destination.push('bhutan');
+destination.pop();
+
+if(destination[1] === 'sajek'){
+
+}
+else if(destination[4] === 'coxbazar'){
+
+}
+else{
+    
+}
