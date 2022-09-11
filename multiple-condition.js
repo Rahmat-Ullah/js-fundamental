f35ab72c3c7db925bd12503acb7712ad15ac582f
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-11 13:01:46 +0600

    multiple condition practiced

---
+++ multiple-condition.js
@@ -0,0 +1,10 @@
+var isGraduated = true;
+var salary = 73000;
+var car = 0;
+
+if(isGraduated === true && salary > 50000 && car == 0){
+    console.log('esho biye kore feli');
+}
+else{
+    console.log('tor kopale biya nai');
+}
